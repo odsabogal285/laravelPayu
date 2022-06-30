04eb52cd4e61f196043ef48a5471630a57537af0
Oscar Sabogal <oscarsabogal285@gmail.com>
2022-06-30 16:16:28 -0500

    Modificación verify token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/confirmacion',
-
+        'https://serene-beach-76968.herokuapp.com/confirmacion',
     ];
 }
