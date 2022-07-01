b101075119cd1704c6bd848fcbf5f56081beb75b
Oscar Sabogal <oscarsabogal285@gmail.com>
2022-07-01 12:15:22 -0500

    Change routes confimation

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/', function () {
 
 Route::post('/billing', [BillController::class, 'index'])->name('billing');
 Route::get('/respuesta', [BillController::class, 'respuesta'])->name('respuesta');
-Route::post('/confirmacion', [BillController::class, 'confirmacion'])->name('confirmacion');
-//Route::post('/confirmacion', [PaymentAttemptController::class, 'confirmacion'])->name('confirmacion');
+//Route::post('/confirmacion', [BillController::class, 'confirmacion'])->name('confirmacion');
+Route::post('/confirmacion', [PaymentAttemptController::class, 'confirmacion'])->name('confirmacion');
 Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
